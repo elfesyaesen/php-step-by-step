b4d1d00eb8c0887de5f1713db50d051a73b65958
EFE ESEN (Elfesya) <web.elfesyaesen@gmail.com>
2023-04-29 14:55:48 +0300

    update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $new_step = $step +1 ;
 $take     = 250;
 $skip     = $step * 250;
 
-$products = $app->getProducts($skip, $take);
+$products = getProducts($skip, $take);
 if ($products) {
   foreach ($products as $key => $product) {
     // Kodlarınız...
@@ -17,3 +17,13 @@ if ($products) {
   echo"<script>location.href='./".$new_step."'</script>";
   exit;
 } else { print 'İşlem yapılacak veri bulunamadı...'; }
+
+function getProducts($pdo, $skip = 0, $take = 0) {
+  $stmt = $pdo->prepare("SELECT * FROM products WHERE LIMIT :limit OFFSET :offset");
+  $stmt->bindValue(':limit', (int) $skip, PDO::PARAM_INT);
+  $stmt->bindValue(':offset', (int) $take, PDO::PARAM_INT);
+  $stmt->execute();
+  $response = $stmt->fetchAll();
+  
+  if($response) { return $response; } else { return false; }
+}
